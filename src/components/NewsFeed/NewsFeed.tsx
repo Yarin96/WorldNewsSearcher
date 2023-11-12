11da5469612494276f4fa47989dedf71d7600ce6
Yarin96 <yarin0008@gmail.com>
2023-11-12 16:05:14 +0200

    add custom hook

--- src/components/NewsFeed/NewsFeed.tsx
+++ src/components/NewsFeed/NewsFeed.tsx
@@ -16,21 +16,29 @@ type TheGuardianArticleType = {
 };
 
 const NewsFeed = () => {
-  const [articleList, setArticleList] = useState<TheGuardianArticleType[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [articleList, setArticleList] = useState<any[]>([]);
 
-  const { data, error } = useAPIFetcher(
-    "https://content.guardianapis.com/search?api-key=3c191f36-fd67-4548-b174-7869ebc458e7"
-  );
+  const { data, error, isLoading } = useAPIFetcher([
+    "https://content.guardianapis.com/search?api-key=3c191f36-fd67-4548-b174-7869ebc458e7",
+    "https://newsapi.org/v2/everything?q=Apple&from=2023-11-12&sortBy=popularity&apiKey=34b87083a4eb404ca6842d44e7a0e85a",
+    "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=d45lnRXfEusOYGrhiU9LZFJl3AzG7R24",
+  ]);
 
   useEffect(() => {
     if (data) {
-      setArticleList(data);
+      const guardianArticles = data[0]?.results || [];
+      const newsApiArticles = data[1]?.articles || [];
+      const nyTimesArticles = data[2]?.response?.docs || [];
+
+      const mergedList = [
+        ...guardianArticles,
+        ...newsApiArticles,
+        ...nyTimesArticles,
+      ];
+      setArticleList(mergedList);
     } else if (error) {
       console.error("Error fetching articles:", error);
     }
-
-    setIsLoading(false);
   }, [data, error]);
 
   return (
--- src/utils/useAPIFetcher.tsx
+++ src/utils/useAPIFetcher.tsx
@@ -1,24 +1,58 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 
-const useAPIFetcher = (url: string) => {
-  const [data, setData] = useState(null);
-  const [error, setError] = useState<unknown>(null);
+type TheGuardianArticleType = {
+  id: string;
+  type: string;
+  sectionId: string;
+  sectionName: string;
+  webPublicationDate: string;
+  webTitle: string;
+  webUrl: string;
+  isHosted: boolean;
+  pillarId: string;
+  pillarName: string;
+};
+
+type NewsAPIArticleType = {};
+
+type NYTimesArticleType = {};
+
+type ArticleType =
+  | TheGuardianArticleType
+  | NewsAPIArticleType
+  | NYTimesArticleType;
+
+interface ApiResponse<T> {
+  data: T | null;
+  error: Error | null;
+  isLoading: boolean;
+}
+
+const useAPIFetcher = <T,>(apiUrls: string[]): ApiResponse<T[]> => {
+  const [data, setData] = useState<T[] | null>(null);
+  const [error, setError] = useState<Error | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(url);
-        const result = await response.json();
-        setData(result.response.results);
-      } catch (error) {
+        setIsLoading(true);
+        const promises = apiUrls.map((url) => axios.get<T>(url));
+        const responses = await Promise.all(promises);
+        const responseData = responses.map((response) => response.data);
+        setData(responseData);
+      } catch (error: any | unknown) {
         setError(error);
+      } finally {
+        setIsLoading(false);
       }
     };
 
     fetchData();
-  }, [url]);
+  }, [apiUrls]);
 
-  return { data, error };
+  return { data, error, isLoading };
 };
 
 export default useAPIFetcher;
